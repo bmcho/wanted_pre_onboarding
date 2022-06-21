e77b1714d3bd5e330f9e1268483293df9c47ffb9
bmcho <bmcho91@naver.com>
2022-06-21 16:26:35 +0900

    Add - #1 model End
    유저, 회사, 채용공고, 지원내역 model 작성완료

--- onboarding/user/models.py
+++ onboarding/user/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.TextChoices):
+    PULLSTACK = "PullStack"
+    BACKEND = "BackEme"
+    FRONTEND = "FrontEnd"
+    NONE = "None"
+
+
+class TimeStampedModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(TimeStampedModel):
+
+    username = models.CharField(max_length=20, null=False)
+    email = models.EmailField(unique=True, error_messages={"unique": "이미 사용중인 이메일입니다."})
+    position = models.CharField(max_length=10, choices=Position.choices, default=Position.NONE)
+    # job_posting = models.ManyToManyField("JopPosting")
+
+
+class ApplyPosting(TimeStampedModel):
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    posting = models.ForeignKey("jopPosting.JopPosting", on_delete=models.CASCADE)
